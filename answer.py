58405109887dba7a1495d2751bea5c5ac7af6bc8
somaSzolyak <soma.szolyak@gmail.com>
2017-08-16 13:20:07 +0200

    pep8 corrected

--- answer.py
+++ answer.py
@@ -1,4 +1,4 @@
-#from flask import Flask, render_template, redirect, request, session, url_for
+# from flask import Flask, render_template, redirect, request, session, url_for
 from datetime import datetime
 import common
 import csv
