4172446a9be410d3f882acb05ea23e00c676933a
somaSzolyak <soma.szolyak@gmail.com>
2017-08-17 11:16:31 +0200

    editing a question works

--- server.py
+++ server.py
@@ -44,7 +44,9 @@ def question_edit(question_id):
     keys_of_form = request.form.keys()
     for key in keys_of_form:
         print(key)
-        edit_question_dict.update({key: request.form[key]})
+        edit_question_dict.update({key: request.form.get(key)})
+    print(edit_question_dict)
+    print("question_id type: {}".format(type(question_id)))
     return question.edit_question(question_id, edit_question_dict)
 
 
