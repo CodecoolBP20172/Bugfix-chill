a776ce30785d0a7dc3b4255d7cb5249d8905a54d
gabor-szeles <szelesgbr@gmail.com>
2017-08-31 14:06:13 +0200

    edit comment working

--- common.py
+++ common.py
@@ -47,6 +47,41 @@ def ordering(cursor, criteria, order, limit):
 
 @connection_handler
 def remove_comment(cursor, comment_id):
+    question_id = get_question_id(comment_id)
+    cursor.execute("""DELETE FROM comment
+                      WHERE id = (%s);""", (comment_id,))
+    return redirect("/question/{}".format(question_id))
+
+
+def url_validation(criteria, order):
+    valid_criteria = ['submission_time', 'view_number', 'vote_number']
+    valid_order = ['ASC', 'DESC']
+    if criteria and order and (criteria in valid_criteria and order in valid_order):
+        return True
+    else:
+        return False
+
+
+@connection_handler
+def edit_comment(cursor, comment_id):
+    cursor.execute("""SELECT *
+                      FROM comment
+                      WHERE id = (%s);""", (comment_id,))
+    comment_data = cursor.fetchall()
+    return render_template("form.html", comment_id=comment_id, comment_data=comment_data[0], form_type="edit_comment")
+
+
+@connection_handler
+def submit_comment_edited(cursor, comment_id, message):
+    question_id = get_question_id(comment_id)
+    cursor.execute("""UPDATE comment
+                      SET message = (%s), edited_count = edited_count +1
+                      WHERE id = (%s);""", (message, comment_id))
+    return redirect("/question/{}".format(question_id))
+
+
+@connection_handler
+def get_question_id(cursor, comment_id):
     cursor.execute("""SELECT question_id
                       FROM comment
                       WHERE id= (%s);""", (comment_id,))
@@ -61,15 +96,4 @@ def remove_comment(cursor, comment_id):
                           WHERE id = (%s);""", (answer_id[0]["answer_id"],))
         question_id = cursor.fetchall()
     question_id = question_id[0]["question_id"]
-    cursor.execute("""DELETE FROM comment
-                      WHERE id = (%s);""", (comment_id,))
-    return redirect("/question/{}".format(question_id))
-
-
-def url_validation(criteria, order):
-    valid_criteria = ['submission_time', 'view_number', 'vote_number']
-    valid_order = ['ASC', 'DESC']
-    if criteria and order and (criteria in valid_criteria and order in valid_order):
-        return True
-    else:
-        return False
+    return question_id
