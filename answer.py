f97f637a76aa037607cba5760621418d12037661
gabor-szeles <szelesgbr@gmail.com>
2017-08-31 13:26:12 +0200

    bugfixes in answer functions, voting and delete wont affect viewcount anymore

--- answer.py
+++ answer.py
@@ -5,37 +5,57 @@ from common import *
 
 @connection_handler
 def post_an_answer(cursor, question_id, message, image):
-    cursor.execute("INSERT INTO answer (submission_time, vote_number, question_id, message, image) VALUES "
-                   "(%s, %s, %s, %s, %s);", (datetime.now(), 0, question_id, message, image))
+    cursor.execute("""INSERT INTO answer (submission_time, vote_number, question_id, message, image)
+                      VALUES (%s, %s, %s, %s, %s);""", (datetime.now(), 0, question_id, message, image))
     return redirect("/question/{}".format(question_id))
 
 
 @connection_handler
 def delete_answer(cursor, answer_id):
-    cursor.execute("DELETE FROM comment WHERE answer_id = (%s);", (answer_id))
-    cursor.execute("DELETE FROM answer WHERE id = (%s);", (answer_id))
+    cursor.execute("""SELECT question_id
+                      FROM answer
+                      WHERE id = (%s);""", (answer_id,))
+    question_id = cursor.fetchall()
+    cursor.execute("""DELETE FROM comment
+                      WHERE answer_id = (%s);""", (answer_id,))
+    cursor.execute("""DELETE FROM answer
+                      WHERE id = (%s);""", (answer_id,))
+    cursor.execute("""UPDATE question
+                      SET view_number = view_number - 1
+                      WHERE id = (%s);""", (question_id[0]["question_id"],))
 
 
 @connection_handler
 def upvote(cursor, id_, question_id, vote):
-    cursor.execute("SELECT vote_number FROM answer WHERE id = (%s);", (id_))
+    cursor.execute("""SELECT vote_number
+                      FROM answer
+                      WHERE id = (%s);""", (id_,))
     current_vote = cursor.fetchall()
     current_vote = current_vote[0]
     current_vote = current_vote["vote_number"]
     if vote == "up":
         current_vote += 1
-        cursor.execute("UPDATE answer SET vote_number = (%s) WHERE id = (%s);", (current_vote, id_))
+        cursor.execute("""UPDATE answer
+                          SET vote_number = (%s)
+                          WHERE id = (%s);""", (current_vote, id_))
     else:
         current_vote -= 1
-        cursor.execute("UPDATE answer SET vote_number = (%s) WHERE id = (%s);", (current_vote, id_))
+        cursor.execute("""UPDATE answer
+                          SET vote_number = (%s)
+                          WHERE id = (%s);""", (current_vote, id_))
+    cursor.execute("""UPDATE question
+                      SET view_number = view_number - 1
+                      WHERE id = (%s);""", (question_id,))
     return redirect("/question/{}".format(question_id))
 
 
 @connection_handler
 def comment_answer(cursor, answer_id, message):
-    cursor.execute("INSERT INTO comment (question_id, answer_id, message, submission_time, edited_count)"
-                   "VALUES (%s, %s, %s, %s, %s);", (None, answer_id, message, datetime.now(), 0))
-    cursor.execute("SELECT question_id FROM answer WHERE id=%s;", answer_id)
+    cursor.execute("""INSERT INTO comment (question_id, answer_id, message, submission_time, edited_count)"
+                   "VALUES (%s, %s, %s, %s, %s);""", (None, answer_id, message, datetime.now(), 0))
+    cursor.execute("""SELECT question_id
+                      FROM answer
+                      WHERE id=%s;""", (answer_id,))
     question_id = cursor.fetchall()
     question_id = question_id[0]["question_id"]
     return redirect("/question/{}".format(question_id))
