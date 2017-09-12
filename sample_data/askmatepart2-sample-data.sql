2dcc3702ca0fa7545a9f3e0a3f2a6ad12d36b300
Soma Szolyák <soma.szolyak@gmail.com>
2017-09-12 11:34:09 +0200

    added unique to username

--- sample_data/askmatepart2-sample-data.sql
+++ sample_data/askmatepart2-sample-data.sql
@@ -71,7 +71,7 @@ DROP TABLE IF EXISTS public.users;
 DROP SEQUENCE IF EXISTS public.users_id_seq;
 CREATE TABLE users (
     id serial NOT NULL,
-    username VARCHAR(15) NOT NULL,
+    username VARCHAR(15) unique NOT NULL,
     password VARCHAR(10) NOT NULL,
     registration_date timestamp without time zone,
     reputation integer DEFAULT 0
