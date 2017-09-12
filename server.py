a1737fcaa0932d440f7bfd657af8c2b66953e2f4
gabor-szeles <szelesgbr@gmail.com>
2017-09-12 13:45:05 +0200

    existing user error handling added again

--- server.py
+++ server.py
@@ -43,13 +43,14 @@ Functions related to users
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
+    new_user = True
     if request.method == 'POST':
         user_name = request.form.get("user_name")
         password = request.form.get("password")
         new_user = users.register_user(user_name, password)
         if new_user:
             return redirect("/")
-    return render_template("registration.html")
+    return render_template("registration.html", new_user=new_user)
 
 
 
