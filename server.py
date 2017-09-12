3a972c4d2d793377a0ee65e7d4bcada63c64edc9
gabor-szeles <szelesgbr@gmail.com>
2017-09-12 11:07:07 +0200

    login functions added

--- server.py
+++ server.py
@@ -35,6 +35,28 @@ def index_list():
     return question.question_index(criteria, order)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        session['password'] = request.form['password']
+        cursor.execute("""SELECT username, password
+                          FROM users
+                          WHERE username = %s;""", (session['username'],))
+        table = cursor.fetchall()
+        if table:
+            if table[0]['password'] == session['password']:
+                return redirect(url_for('index'))
+    return render_template("login.html")
+
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
 """
 Functins handleing interactions with questions
 """
