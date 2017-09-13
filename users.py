2adbe2a5d43f61ab6b08a629b8bab4b32477f433
Debora Csomos <csomidebi@gmail.com>
2017-09-13 21:48:14 +0200

    deleted password form query

--- users.py
+++ users.py
@@ -30,10 +30,10 @@ def register_user(cursor, user_name, password):
 
 @connection_handler
 def list_users(cursor):
-    cursor.execute('SELECT username, password, registration_date, reputation \
+    cursor.execute('SELECT username, registration_date, reputation \
                     FROM users;')
     user_dict = cursor.fetchall()
-    user_headers = ["username", "password", "registration_date", "reputation"]
+    user_headers = ["username", "registration_date", "reputation"]
     return render_template('user_list.html', user_dict=user_dict, user_headers=user_headers)
 
 
